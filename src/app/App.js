2e5cbbb59217dfecb7a7083c01f589664720bddd
Jeffrey Soriano <jeffsor17@gmail.com>
2020-10-02 21:55:07 -0600

    website component layout added

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Home } from './components/Home';
+import { Navbar } from './components/Navbar';
+import { WebLayout } from './WebLayout';
 
 function App() {
   return (
     <>
-      <Home/>
+      <Navbar />
+      <WebLayout />
     </>
   );
 }
---
+++ src/app/WebLayout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { AboutMe } from './components/AboutMe'
+import { Home } from './components/Home'
+import { Projects } from './components/Projects'
+import { Skills } from './components/Skills'
+import { Slide } from './components/Slide'
+import { Contacts } from './components/Contacts'
+
+export const WebLayout = () => {
+    return (
+        <>
+            <Home />
+            <Slide />
+            <Projects />
+            <Skills />
+            <AboutMe />
+            <Contacts />
+        </>
+    )
+}
