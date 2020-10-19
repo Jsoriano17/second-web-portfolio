aa8c92b70bce1f54555c0c1d04fd847c07fd1aeb
Jeffrey Soriano <jeffsor17@gmail.com>
2020-10-19 04:36:01 -0600

    added back button to about me

--- src/app/components/AboutMe.js
+++ src/app/components/AboutMe.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import ContactIcons from './ContactIcons'
 
 export const AboutMe = () => {
     return (
         <div>
             about me page
+            <br/>
+            <Link to='/'>go back</Link>
         </div>
     )
 }
