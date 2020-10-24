0a48bd711ad455d71e17b8d8f576800cd64fedd9
Jeffrey Soriano <jeffsor17@gmail.com>
2020-10-24 04:22:01 -0600

    added not found page

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import { AboutMe } from './components/AboutMe';
+import NotFound from './NotFound';
 import { WebLayout } from './WebLayout';
 
 function App() {
   return (
-    <>
+    <Switch>
       <Route exact path="/" component={WebLayout} />
-      <Route exact path={'/about'} component={AboutMe}/>
-    </>
+      <Route exact path='/about' component={AboutMe}/>
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
