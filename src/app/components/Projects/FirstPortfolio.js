a024655224fc460f8663bcd40872bba8cf406507
Jeffrey Soriano <jeffsor17@gmail.com>
2020-10-29 04:16:46 -0600

    first portfolio responsive

--- src/app/components/Projects/FirstPortfolio.js
+++ src/app/components/Projects/FirstPortfolio.js
@@ -34,41 +34,41 @@ export default FirstPortfolio
 
 const StyledImg = styled.img`
     position: absolute;
-    width: 1100px;
-    top: 1100px;
+    width: 85.9375vw;
+    top: 85.9375vw;
     right: -36vw;
     z-index: 1;
 `
 const StyledGif = styled.img`
     position: absolute;
-    width: 235px;
-    height: 130px;
-    top: 1670px;
-    right: 312px;
+    width: 18.359375vw;
+    height: 18.8953488372093vh;
+    top: 242.7325581395349vh;
+    right: 24.375vw;
     z-index: 0;
 `
 const ClickBox = styled.img`
     position: absolute;
-    width: 235px;
-    height: 130px;
-    top: 1670px;
-    right: 312px;
+    width: 18.359375vw;
+    height: 18.8953488372093vh;
+    top: 242.7325581395349vh;
+    right: 24.375vw;
     opacity: 0;
     z-index: 3;
 `
 const StyledP = styled.p`
-    font-size: 15px; 
-    margin-top: -55px;
-    margin-left: 735px;
+    font-size:  1.171875vw; 
+    margin-top: -7.994186046511628vh;
+    margin-left: 57.421875vw;
     color: black;
     position: relative;
     z-index: 3;
 `
 const StyledUl = styled.ul`
-    font-size: 17px; 
+    font-size: 1.328125vw; 
     font-weight: bold;
-    margin-top: 21px;
-    margin-left: 980px;
+    margin-top: 3.052325581395349vh;
+    margin-left: 79.6875vw;
     position: relative;
     color: white;
     list-style-type: none;
@@ -76,5 +76,5 @@ const StyledUl = styled.ul`
 `
 
 const StyledLi = styled.li`
-    margin: 9px 0;
+    margin: 1.308139534883721vh 0;
 `
