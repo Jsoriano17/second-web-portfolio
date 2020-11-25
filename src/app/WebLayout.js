934292b6af48707b167e011bd9c04e97190a7d40
Jeffrey Soriano <jeffsor17@gmail.com>
2020-11-25 03:46:15 -0700

    form test

--- src/app/WebLayout.js
+++ src/app/WebLayout.js
@@ -5,30 +5,30 @@ import { Skills } from './components/Skills'
 import styled from 'styled-components'
 import { Navbar } from './components/Navbar'
 import ScrollToTop from "react-scroll-to-top"
-import Contacts from './components/Contacts'
+// import Contacts from './components/Contacts'
 import ContactMobile from './components/ContactMobile'
-import { useMediaQuery } from 'react-responsive'
+// import { useMediaQuery } from 'react-responsive'
 
 export const WebLayout = () => {
-    const isDesktopOrLaptop = useMediaQuery({
-        query: '(min-device-width: 1224px)'
-    })
-    const isBigScreen = useMediaQuery({ query: '(min-device-width: 5000px)' })
-    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
-    const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+    // const isDesktopOrLaptop = useMediaQuery({
+    //     query: '(min-device-width: 1224px)'
+    // })
+    // const isBigScreen = useMediaQuery({ query: '(min-device-width: 5000px)' })
+    // const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+    // const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
     return (
         <Container>
             <Navbar />
             <Home />
             <Projects />
             <Skills />
-            {isDesktopOrLaptop && <>
+            {/* {isDesktopOrLaptop && <>
                 <Contacts />
-            </>}
-            {isTabletOrMobile && isPortrait && <>
-                <ContactMobile />
-            </>}
-            {isBigScreen && <> </>}
+            </>} */}
+            {/* {isTabletOrMobile && isPortrait && <>
+            </>} */}
+            <ContactMobile />
+            {/* {isBigScreen && <> </>} */}
             <ScrollToTop
                 color="white"
                 viewBox="-70 -10 400 300"
