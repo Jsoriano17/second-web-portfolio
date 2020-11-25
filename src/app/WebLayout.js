4314a0bd63032d26d84f4b31d65a55954285a034
Jeffrey Soriano <jeffsor17@gmail.com>
2020-11-25 03:38:36 -0700

    fixed forms for mobile/moved both contacts weblayout.js

--- src/app/WebLayout.js
+++ src/app/WebLayout.js
@@ -5,19 +5,30 @@ import { Skills } from './components/Skills'
 import styled from 'styled-components'
 import { Navbar } from './components/Navbar'
 import ScrollToTop from "react-scroll-to-top"
-// import Contacts from './components/Contacts'
+import Contacts from './components/Contacts'
 import ContactMobile from './components/ContactMobile'
+import { useMediaQuery } from 'react-responsive'
 
 export const WebLayout = () => {
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(min-device-width: 1224px)'
+    })
+    const isBigScreen = useMediaQuery({ query: '(min-device-width: 5000px)' })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+    const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
     return (
         <Container>
             <Navbar />
             <Home />
             <Projects />
             <Skills />
-            {/* <Contacts />
-             */}
-             <ContactMobile />
+            {isDesktopOrLaptop && <>
+                <Contacts />
+            </>}
+            {isTabletOrMobile && isPortrait && <>
+                <ContactMobile />
+            </>}
+            {isBigScreen && <> </>}
             <ScrollToTop
                 color="white"
                 viewBox="-70 -10 400 300"
@@ -28,6 +39,7 @@ export const WebLayout = () => {
                     background: 'black',
                     opacity: 0.7
                 }} />
+
         </Container>
     )
 }
--- src/app/components/Contacts.js
+++ src/app/components/Contacts.js
@@ -2,9 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import ScrollAnimation from 'react-animate-on-scroll';
 import ContactMag from '../assets/magazines/contact-mag.png';
-import ContactMagMobile from '../assets/contact-mag-mobile.png';
 import EnvelopePic from '../assets/envelope.png';
-import MediaQuery from 'react-responsive'
 
 const encode = (data) => {
     return Object.keys(data)
@@ -35,25 +33,19 @@ class Contacts extends React.Component {
         const { name, email, message } = this.state;
         return (
             <>
-                <MediaQuery minDeviceWidth={1224}>
-                    <ScrollAnimation animateIn='animate__fadeIn' animateOnce={true}>
-                        <Container>
-                            <StyledH1 id="contacts">Lets_Work_Together.</StyledH1>
-                            <form style={{ fontSize: '2vw' }} onSubmit={this.handleSubmit}>
-                                <StyledNameInput type="text" name="name" value={name} onChange={this.handleChange} required />
-                                <StyledEmailInput type="email" name="email" value={email} onChange={this.handleChange} required />
-                                <StyledMessageInput name="message" value={message} onChange={this.handleChange} required />
-                                <StyledSubmit type='submit' />
-                            </form>
-                            <Magazine src={ContactMag} />
-                            <Envelope src={EnvelopePic} />
-                        </Container>
-                    </ScrollAnimation>
-                </MediaQuery>
-                <MediaQuery maxDeviceWidth={1224} orientation={'portrait'}>
-                   
-                </MediaQuery>
-                <MediaQuery minDeviceWidth={4000}></MediaQuery>
+                <ScrollAnimation animateIn='animate__fadeIn' animateOnce={true}>
+                    <Container>
+                        <StyledH1 id="contacts">Lets_Work_Together.</StyledH1>
+                        <form style={{ fontSize: '2vw' }} onSubmit={this.handleSubmit}>
+                            <StyledNameInput type="text" name="name" value={name} onChange={this.handleChange} required />
+                            <StyledEmailInput type="email" name="email" value={email} onChange={this.handleChange} required />
+                            <StyledMessageInput name="message" value={message} onChange={this.handleChange} required />
+                            <StyledSubmit type='submit' />
+                        </form>
+                        <Magazine src={ContactMag} />
+                        <Envelope src={EnvelopePic} />
+                    </Container>
+                </ScrollAnimation>
             </>
         )
     }
@@ -145,94 +137,6 @@ const StyledH1 = styled.h1`
     font-family: Impact_Label;
     font-size: 6.640625vw;
     margin-bottom: 30%;
-    color: black;
-    font-weight: 100;
-    text-align: center; 
-`
-const ContainerMobile = styled.div`
-    margin-top: 6%;
-    position: relative; 
-    width: 100%;
-    display: inline-block;
-`
-const StyledNameInputMobile = styled.input`
-    position: absolute;
-    top: 67.2%;
-    left: 35.5%;
-    width: 52%;
-    background: transparent;
-    font-size: 3vw;
-    border: none;
-    z-index: 1; 
-    outline: none;
-
-    -webkit-box-shadow: none;
-    -moz-box-shadow: none;
-    box-shadow: none;
-`
-const StyledEmailInputMobile = styled.input`
-    top: 88.5%;
-    left: 31%;
-    position: absolute;
-    width: 56%;
-    background: transparent;
-    font-size: 3vw;
-    border: none;
-    z-index: 1;  
-    outline: none;
-
-    -webkit-box-shadow: none;
-    -moz-box-shadow: none;
-    box-shadow: none;
-`
-const StyledMessageInputMobile = styled.textarea`
-    left: 31%;
-    top: 98%;
-    width: 54%;
-    height: 19%;
-    position: absolute;
-    background: transparent;
-    font-size: 3vw;
-    border: none;
-    resize: none;
-    padding:  0.7267441860465116% 0.390625%;
-    z-index: 1;  
-    outline: none;
-
-    -webkit-box-shadow: none;
-    -moz-box-shadow: none;
-    box-shadow: none;
-`
-const StyledSubmitMobile = styled.input`
-    position: absolute;
-    z-index: 1;    
-    width: 12%; 
-    border: 1px solid black;
-    background: transparent;
-    padding: 0.5% 0 ;
-    top: 120%;
-    font-size: 3vw;
-    left: 75.5%;
-    cursor: pointer;
-`
-const MagazineMobile = styled.img`
-    width: 100%;
-    position: absolute;
-    top: 30%; 
-    left: 0%;
-    z-index: 0;
-`
-const StyledH1Mobile = styled.h1`
-    font-family: Impact_Label;
-    font-size: 15vw;
-    color: black;
-    font-weight: 100;
-    text-align: center; 
-`
-const StyledH1TwoMobile = styled.h1`
-    font-family: Impact_Label;
-    font-size: 13vw;
-    margin-bottom: 85%;
     color: black;
     font-weight: 100;
     text-align: center; 
