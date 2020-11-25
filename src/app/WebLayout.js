22d1dbf1b3338e2492d1b33e9293efedfa9c0e74
Jeffrey Soriano <jeffsor17@gmail.com>
2020-11-25 03:53:58 -0700

    testing forms

--- src/app/WebLayout.js
+++ src/app/WebLayout.js
@@ -5,30 +5,34 @@ import { Skills } from './components/Skills'
 import styled from 'styled-components'
 import { Navbar } from './components/Navbar'
 import ScrollToTop from "react-scroll-to-top"
-// import Contacts from './components/Contacts'
+import Contacts from './components/Contacts'
 import ContactMobile from './components/ContactMobile'
-// import { useMediaQuery } from 'react-responsive'
+import { useMediaQuery } from 'react-responsive'
 
 export const WebLayout = () => {
-    // const isDesktopOrLaptop = useMediaQuery({
-    //     query: '(min-device-width: 1224px)'
-    // })
-    // const isBigScreen = useMediaQuery({ query: '(min-device-width: 5000px)' })
-    // const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
-    // const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(min-device-width: 1224px)'
+    })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+    const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+
+    const chooseContactComponent = () => {
+        if (isDesktopOrLaptop) {
+            return <Contacts />
+        } else if (isTabletOrMobile && isPortrait) {
+            return <ContactMobile />
+        } else {
+            return <></>
+        }
+    }
+
     return (
         <Container>
             <Navbar />
             <Home />
             <Projects />
             <Skills />
-            {/* {isDesktopOrLaptop && <>
-                <Contacts />
-            </>} */}
-            {/* {isTabletOrMobile && isPortrait && <>
-            </>} */}
-            <ContactMobile />
-            {/* {isBigScreen && <> </>} */}
+            {chooseContactComponent()}
             <ScrollToTop
                 color="white"
                 viewBox="-70 -10 400 300"
